c1aaba81f96853aa441efd9fec9be5696f56b54d
MrAlexol <Alex02072002@yandex.ru>
2022-04-24 22:24:26 +0300

    Upgrade generation algorithm

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ class MyFile
     @file_name = file_name # string
     # raise StandardError, "File #{@file_name} exists!" if File.exist?(file_name)
 
-    case output_type
+    case output_type # string
     when 'insert'
       @output_type = :insert
     when 'copy'
@@ -26,31 +26,33 @@ class MyFile
     "Output file: #{@file_name}\nOutput type: #{@output_type}\nFields:\n#{@fields.inspect}"
   end
 
+  def record_to_s(record)
+    case @output_type
+    when :insert
+      result = '('
+      result << record.inject('') { |acc, hsh| acc << "'#{generate_value(hsh[1])}', " }
+      result.gsub(/, $/, ')').gsub(/^/, "\t")
+    when :copy
+      result = record.inject('') { |acc, hsh| acc << "#{generate_value(hsh[1])}|" }
+      result.gsub(/\|$/, '')
+    end
+  end
+
   def generate(number)
     raise StandardError, 'Fields were not set!' if @fields.nil?
 
     f = File.new(@file_name, 'w')
 
-    case @output_type
-    when :insert
-      f.write("INSERT INTO \"my table\"\n\t(\"#{@fields.keys.join('", "')}\")\nVALUES\n")
-    when :copy
+    f.write("INSERT INTO \"my table\"\n\t(\"#{@fields.keys.join('", "')}\")\nVALUES\n") if @output_type == :insert
 
-    end
-
-    # record = @fields.inject('') { |acc, hsh| acc << "#{generate_value(hsh[1])}#{delim}" }
     number.times do |current_number|
-      case @output_type
-      when :insert
-        record = "\t("
-        record << @fields.inject('') { |acc, hsh| acc << "'#{generate_value(hsh[1])}', " }
-        record.gsub!(/, $/, '),')
-        record.gsub!(/,$/, ';') if current_number == number - 1
-      when :copy
-        record = @fields.inject('') { |acc, hsh| acc << "#{generate_value(hsh[1])}|" }
-        record.gsub!(/\|$/, '')
-      end
-      f.puts(record)
+      string = record_to_s(create_record)
+      string << if current_number < number - 1
+                  ','
+                else
+                  ';'
+                end
+      f.puts(string)
     end
 
     f.close
@@ -58,12 +60,14 @@ class MyFile
 
   protected
 
-  def generate_value(key)
-    case key
+  def generate_value(field_value)
+    case field_value
     when /^random.*/
-      arg = key.gsub(/\d+/)
+      # random number in field
+      arg = field_value.gsub(/\d+/)
       rand(arg.next.to_i..arg.next.to_i).to_s
     when 'fio'
+      # full name (russian)
       case rand(0..1)
       when 0
         "#{@surnames_f.sample} #{@names_f.sample} #{@patronymics_f.sample}"
@@ -71,21 +75,12 @@ class MyFile
         "#{@surnames_m.sample} #{@names_m.sample} #{@patronymics_m.sample}"
       end
     else
-      # random number
-      digits = key.gsub(/%d{\d+, *\d+}/)
-      rand_numbers = digits.inject([]) do |acc, subplace|
-        digit = subplace.gsub(/\d+/)
-        acc << rand(digit.next.to_i..digit.next.to_i)
-      end
-      result = key.clone
-      rand_numbers.map { |elem| result.sub!(/%d{\d+, *\d+}/, elem.to_s) }
-
-      # known field
-      # TODO
-      result
+      # random number in string field
+      number_sub(field_value)
     end
   end
 
+  # arrays of full names initialization
   def initialize_arrays
     @surnames_f = IO.readlines('surnames_female.dat', chomp: true)
     @names_f = IO.readlines('names_female.dat', chomp: true)
@@ -93,5 +88,23 @@ class MyFile
     @surnames_m = IO.readlines('surnames_male.dat', chomp: true)
     @names_m = IO.readlines('names_male.dat', chomp: true)
     @patronymics_m = IO.readlines('patronymics_male.dat', chomp: true)
+  end
+
+  def create_record
+    @fields.each_with_object({}) do |field, acc|
+      acc[field[0]] = generate_value(field[1])
+    end
+  end
+
+  def number_sub(key)
+    digits = key.gsub(/%d{\d+, *\d+}/)
+    rand_numbers = digits.inject([]) do |acc, subplace|
+      digit = subplace.gsub(/\d+/)
+      acc << rand(digit.next.to_i..digit.next.to_i)
+    end
+    result = key.clone
+    rand_numbers.map { |elem| result.sub!(/%d{\d+, *\d+}/, elem.to_s) }
+
+    result
   end
 end
--- user.rb
+++ user.rb
@@ -1,8 +1,9 @@
 require './main'
 
-file = MyFile.new('F.out', 'copy')
-file.add_field('Номер', 'random 1..20')
+file = MyFile.new('F.out', 'insert')
 file.add_field('ФИО', 'fio')
-file.add_field('Информация', 'Телефон: %d{20, 500}-%d{10, 50}')
+file.add_field('Дата рождения', '%d{1, 28}.%d{1, 12}.%d{1700, 1980}')
+file.add_field('Дата смерти', '%d{1, 28}.%d{1, 12}.%d{1740, 2022}')
+file.add_field('Ссылка', 'random 1..50')
 
 file.generate(100)
